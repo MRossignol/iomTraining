27826dafdbfd9bcea2c16a906fd45e6528e4095d
Mathieu Lagrange <mathieu.lagrange@cnrs.fr>
2017-08-03 14:11:03 +0200

    up to new layout

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 
-var app = angular.module('iomTraining', ['rzModule', 'ui.bootstrap']);
+var app = angular.module('iomTraining', ['rzModule', 'ui.bootstrap', 'dark-sky']);
 
 app.config(['$compileProvider',
 		 function($compileProvider) {
 		     $compileProvider.aHrefSanitizationWhitelist(/^\s*(https?|ftp|mailto|blob|file):/);
 		 }]);
+
+app.config(['darkSkyProvider', function(darkSkyProvider) {
+		  	darkSkyProvider.setApiKey('df2d7141740701587a1cde5b5d340379');
+		 	}]);
--- iomController.js
+++ iomController.js
@@ -1,56 +1,9 @@
-function BufferLoader(context, urlList, callback) {
-	this.context = context;
-    this.urlList = urlList;
-    this.onload = callback;
-    this.bufferList = new Array();
-    this.loadCount = 0;
-}
-
-BufferLoader.prototype.loadBuffer = function(url, index) {
-    var request = new XMLHttpRequest();
-    request.open("GET", url, true);
-    request.responseType = "arraybuffer";
-
-    var loader = this;
-
-    request.onload = function() {
-        loader.context.decodeAudioData(
-            request.response,
-            function(buffer) {
-                if (!buffer) {
-                    alert('error decoding file data: ' + url);
-                    return;
-                }
-                loader.bufferList[index] = buffer;
-                if (++loader.loadCount == loader.urlList.length)
-                    loader.onload(loader.bufferList);
-            }
-        );
-    }
-
-    request.onerror = function() {
-        alert('BufferLoader: XHR error');
-    }
-
-    request.send();
-}
-
-BufferLoader.prototype.load = function() {
-    for (var i = 0; i < this.urlList.length; ++i)
-        this.loadBuffer(this.urlList[i], i);
-}
-
 app.controller("iomController", function($scope, $timeout, $interval, $window) {
   var audioContext = null;
-  var analyzer = null;
   var meter = null;
   var timing = 0;
-  var detector = null;
   var bufferLoader = null;
-  $scope.sensitivity = 0;
-  $scope.power = 0;
-  $scope.clickDetected = 0;
-  $scope.move = 0;
+
   $scope.wakeLockEnabled = false;
   $scope.wakeValue = "Wake Lock is disabled";
 
@@ -65,82 +18,52 @@ app.controller("iomController", function($scope, $timeout, $interval, $window) {
   $scope.legNumber = 1;
   $scope.nbUpStore = 0;
   $scope.nbDownStore = 0;
+  $scope.showSettings = false;
+  $scope.clickRight = 0;
+  $scope.clickLeft = 0;
 
   $scope.legSlider = {
-    value: 3,
-    options: {
-      floor: 1,
-      ceil: 10,
-      step: 1,
-      showTicks: true,
-      showSelectionBar: true
-    }
-  };
-
-  $scope.clickSlider = {
-    value: 5,
-    options: {
-      floor: 1,
-      ceil: 5,
-      step: 1,
-      showTicks: true,
-      showSelectionBar: true
-    }
-  };
-
-  $scope.sensitivitySlider = {
-    value: 1.2,
-    options: {
-      floor: 1,
-      ceil: 5,
-      step: 0.05,
-      precision: 1,
-      showTicks: .2,
-      showSelectionBar: true
-    }
-  };
-
-  $scope.powerSlider = {
     value: 2,
     options: {
-      floor: 1,
+      floor: 0,
       ceil: 10,
-      step: 0.2,
-      precision: 1,
-      showTicks: .5,
+      step: 1,
+      showTicks: true,
       showSelectionBar: true
     }
   };
+
+  $scope.startSlider = {
+    value: 5,
+    options: {
+      floor: 0,
+      ceil: 5,
+      step: 1,
+      showTicks: true,
+      showSelectionBar: true
+    }
+  };
+
+
+
 
   var noSleep = new NoSleep();
 
-  // function onError(){console.log('Unable to load audio data');}
-
-  // function loadSound(url) {
-  //   var request = new XMLHttpRequest();
-  //   request.open('GET', url, true);
-  //   request.responseType = 'arraybuffer';
-  //
-  //   // Decode asynchronously
-  //   request.onload = function() {
-  //     audioContext.decodeAudioData(request.response, function(buffer) {
-  //       okBuffer = buffer;
-  //     }, onError);
-  //   }
-  //   request.send();
-  // }
-
   function loadSounds() {
+    window.AudioContext = window.AudioContext || window.webkitAudioContext;
+    // grab an audio context
+    audioContext = new AudioContext();
+
     bufferLoader = new BufferLoader(
-       audioContext,
-       [
-         'sounds/ok.wav',
-         'sounds/tick.wav',
-         'sounds/startVrc.webm',
-       ],
-       finishedLoading
-       );
-     bufferLoader.load();
+      audioContext,
+      [
+        'sounds/ok.wav',
+        'sounds/tick.wav',
+        'sounds/startVrc.webm',
+      ],
+      finishedLoading
+    );
+    bufferLoader.load();
   }
 
   function finishedLoading(){
@@ -148,12 +71,12 @@ app.controller("iomController", function($scope, $timeout, $interval, $window) {
   }
 
   function playSound(id) {
-  var source = audioContext.createBufferSource(); // creates a sound source
-  // console.log(buffer);
-  source.buffer = bufferLoader.bufferList[id];                    // tell the source which sound to play
-  source.connect(audioContext.destination);       // connect the source to the context's destination (the speakers)
-  source.start(0);
-}
+    var source = audioContext.createBufferSource(); // creates a sound source
+    // console.log(buffer);
+    source.buffer = bufferLoader.bufferList[id];                    // tell the source which sound to play
+    source.connect(audioContext.destination);       // connect the source to the context's destination (the speakers)
+    source.start(0);
+  }
 
 
   $scope.wake = function() {
@@ -169,174 +92,137 @@ app.controller("iomController", function($scope, $timeout, $interval, $window) {
     }
   }
 
-
-
-    $scope.startFunc = function(store = false) {
-      if ($scope.started) {
-        console.log('stop')
-        $scope.started = false;
-        $scope.infoText = 'start';
-        if (store) {
+  $scope.startFunc = function(store = false) {
+    if ($scope.started) {
+      console.log('stop')
+      $scope.started = false;
+      $scope.infoText = 'start';
+      if (store) {
         $scope.upStore = $scope.upStore.concat($scope.upDurations);
         $scope.downStore = $scope.downStore.concat($scope.downDurations);
         $scope.nbUpStore = $scope.upStore.length;
         $scope.nbDownStore = $scope.downStore.length;
-        }
-        }
-      else {
-        console.log('start');
-        $scope.infoText = 'stop';
-        $scope.started = true;
-        $scope.legNumber = 1;
-        $scope.upDurations = [];
-        $scope.downDurations = [];
-        $scope.currentDuration = 0;
-        $scope.upLeg = true;
-        $scope.legType = 'Up';
-        timing = new Date().getTime();
       }
     }
-
-    nextLeg = function() {
-      if ($scope.started){
-        var newTiming = new Date().getTime();
-        var duration = newTiming-timing;
-        timing = newTiming;
-
-        if ($scope.upLeg) {
-          $scope.upLeg = false;
-          $scope.legType = 'Down';
-          $scope.upDurations.push(duration);
-        }
-        else {
-          $scope.upLeg = true;
-          $scope.legNumber++;
-          $scope.legType = 'Up';
-          $scope.downDurations.push(duration);
-        }
-        if (duration>$scope.maxDuration) {
-          $scope.maxDuration = duration;
-        }
-        if ($scope.legNumber>$scope.legSlider.value) {
-          $scope.startFunc(1);
-        }
-      }
+    else {
+      console.log('start');
+      $scope.infoText = 'stop';
+      $scope.started = true;
+      $scope.legNumber = 1;
+      $scope.upDurations = [];
+      $scope.downDurations = [];
+      $scope.currentDuration = 0;
+      $scope.upLeg = true;
+      $scope.legType = 'Up';
+      timing = new Date().getTime();
     }
-
-    var update = function(){
-      if(detector) {
-        detector.sensitivityThreshold = $scope.sensitivitySlider.value;
-        detector.powerThreshold = $scope.powerSlider.value;
-        $scope.power = detector.power.toFixed(2);
-        $scope.sensitivity = detector.sensitivity.toFixed(2);
-        $scope.powerAverage = detector.powerAverage/detector.powerBuffer.length;
-        $scope.powerAverage = $scope.powerAverage.toFixed(4);
-        $scope.sensitivityAverage = detector.sensitivityAverage/detector.sensitivityBuffer.length;
-        $scope.sensitivityAverage = $scope.sensitivityAverage.toFixed(2);
-        $scope.powerMax = detector.powerMax.toFixed(4);
-        $scope.sensitivityMax = detector.sensitivityMax.toFixed(2);
-
-      if (detector.clickDetected && detector.active) {
-        detector.active = false;
-
-        if ($scope.clickDetected == 0) {
-          $timeout(function(){$scope.clickDetected = 0;}, 2000);
-        }
-        $scope.clickDetected += 1;
-        if ($scope.clickDetected>=$scope.clickSlider.value) {
-          nextLeg();
-          playSound(0);
-        }
-        $timeout(function(){detector.active = true; detector.clickDetected = 0;}, 100);
-      }
-    }
-    $scope.currentDuration = new Date().getTime()-timing;
-      $timeout(function(){update()}, 100);
-    }
-
-    launchDetector = function(){
-    // monkeypatch Web Audio
-    window.AudioContext = window.AudioContext || window.webkitAudioContext;
-    // grab an audio context
-    audioContext = new AudioContext();
-    // Attempt to get audio input
-    try {
-      // monkeypatch getUserMedia
-      navigator.getUserMedia =
-      navigator.getUserMedia ||
-      navigator.webkitGetUserMedia ||
-      navigator.mozGetUserMedia;
-
-      // ask for an audio input
-      navigator.getUserMedia(
-        {
-          "audio": {
-            "mandatory": {
-              "googEchoCancellation": "false",
-              "googAutoGainControl": "false",
-              "googNoiseSuppression": "false",
-              "googHighpassFilter": "false"
-            },
-            "optional": []
-          },
-        }, gotStream, didntGetStream);
-      } catch (e) {
-        alert('getUserMedia threw exception :' + e);
-      }
-
-      function didntGetStream() {
-        alert('Stream generation failed.');
-      }
-
-      var mediaStreamSource = null;
-      function gotStream(stream) {
-        // Create an AudioNode from the stream.
-        mediaStreamSource = audioContext.createMediaStreamSource(stream);
-
-        // cw version: Create a new volume meter and connect it.
-        detector = createDetector(audioContext);
-
-        mediaStreamSource.connect(detector);
-
-        // kick off the visual updating
-  update();
-      }
   }
 
-    launchDetector();
+  nextLeg = function() {
+    if ($scope.started){
+      var newTiming = new Date().getTime();
+      var duration = newTiming-timing;
+      timing = newTiming;
 
-   loadSounds();
-    // $timeout(function(){playSound(okBuffer)}, 500);
+      if ($scope.upLeg) {
+        $scope.upLeg = false;
+        $scope.legType = 'Down';
+        $scope.upDurations.push(duration);
+      }
+      else {
+        $scope.upLeg = true;
+        $scope.legNumber++;
+        $scope.legType = 'Up';
+        $scope.downDurations.push(duration);
+      }
+      if (duration>$scope.maxDuration) {
+        $scope.maxDuration = duration;
+      }
+      if ($scope.legNumber>$scope.legSlider.value) {
+        $scope.startFunc(1);
+      }
+    }
+  }
 
-    $scope.doClick = function(event){
-        if (event.offsetX<document.getElementById('svg').clientWidth/2) {
-          $scope.clickDetected=1;
+  loadSounds();
+
+  function handleClick () {
+
+    if ($scope.clickLeft) {
+      if ($scope.clickRight) {
+        console.log('double alternate');
+      }
+      else {
+        if ($scope.clickLeft>1) {
+          console.log('double left');
         }
         else {
-          $scope.clickDetected=0;
+          console.log('left');
         }
-    };
-
-
-    document.onkeydown = function(e) {
-      switch (e.keyCode) {
-        case 37:
-        $scope.sensitivity = 1;
-        break;
-        case 38:
-        $scope.sensitivity += .1;
-        break;
-        case 40:
-        $scope.sensitivity -= .1;
-        $scope.sensitivity = Math.max($scope.sensitivity, 1);
-        break;
-        case 76: // l
-        nextLeg();
-        break;
-        case 83: // s
-        $scope.startFunc();
-        break;
       }
-      //  console.log(e.keyCode)
-    };
-  });
+    }
+      else {
+        if ($scope.clickRight) {
+          if ($scope.clickRight>1) {
+            console.log('double right');
+          }
+          else {
+            console.log('right');
+          }
+        }
+      }
+
+    $scope.clickLeft = 0;
+    $scope.clickRight = 0;
+  }
+
+  function gotClick(left) {
+    if (!$scope.clickLeft && !$scope.clickRight) {
+      $timeout(function(){handleClick()}, 500);
+    }
+    if (left)
+    $scope.clickLeft+=1;
+    else {
+      $scope.clickRight+=1;
+    }
+  }
+  // $timeout(function(){playSound(okBuffer)}, 500);
+
+  $scope.doClick = function(event){
+    if (event.offsetX<document.getElementById('raceDisplay').clientWidth/2) {
+      gotClick(true);
+    }
+    else {
+      gotClick(false);
+    }
+    // console.log(event);
+  };
+
+  document.onkeydown = function(e) {
+    switch (e.keyCode) {
+      case 37:
+      $scope.sensitivity = 1;
+      break;
+      case 38:
+      $scope.sensitivity += .1;
+      break;
+      case 40:
+      $scope.sensitivity -= .1;
+      $scope.sensitivity = Math.max($scope.sensitivity, 1);
+      break;
+      case 76: // l
+      nextLeg();
+      break;
+      case 83: // s
+      $scope.startFunc();
+      break;
+      case 33: // p up
+      gotClick(true);
+      break;
+      case 34: // p down
+      gotClick(false);
+      break;
+    }
+    console.log(e.keyCode)
+  };
+});
