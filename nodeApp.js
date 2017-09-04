3c5d79ccbd1cf1b3c663b4b89f9ac21ec65d6296
Lagrange <lagrange@ircam.fr>
2017-09-04 15:38:59 +0200

    node project

---
+++ nodeApp.js
@@ -0,0 +1,52 @@
+var express = require('express');
+var app = express();
+var fs = require('fs');
+var bodyParser = require('body-parser');
+var multer  = require('multer');
+var exec = require('child_process').exec;
+
+app.all('/*', function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  next();
+});
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+var baseDir = __dirname.replace(/server\/?/, "");
+var uploadDir = baseDir.replace(/\/?$/, "/upload/");
+
+var currentSkillDay = new Date();
+var currentSeriesDay = new Date();
+var currentSeriesId = 0;
+
+app.get(('/getSeriesId', function (req, res) {
+  var newDay = new Date();
+  if (newDay.toDateString() != currentDay.toDateString()) {
+  currentDay = newDay;
+    currentSeriesId = 0;
+  }
+  else {
+    currentSeriesId++;
+  }
+  res.send("<p>"+currentSeriesId+"</p>");
+  console.log(currentSeriesId);
+})
+
+app.get('/sendSeries', function (req, res) {
+    var fileName = 'data/series/'+Date.now().toString()+currentSeriesId.toString()+'.json';
+    console.log("received Series");
+    res.send(JSON.stringify(req.query));
+    fs.writeFile(fileName, JSON.stringify(req.query));
+})
+
+app.get('/toto', function (req, res) {
+    res.send("<p>Bonjour Toto</p>");
+})
+
+var server = app.listen(4000, function () {
+    var host = server.address().address
+    var port = server.address().port
+
+    console.log("Example app listening at http://%s:%s", host, port)
+});
